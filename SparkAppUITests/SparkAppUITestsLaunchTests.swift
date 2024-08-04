70eb98476a6e9e3240a303797222391b73476c16
fbablu <fardeeneb@gmail.com>
2024-08-04 12:34:25 -0400

    Initial Commit

---
+++ SparkAppUITests/SparkAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SparkAppUITestsLaunchTests.swift
+//  SparkAppUITests
+//
+//  Created by Fardeen Bablu on 8/4/24.
+//
+
+import XCTest
+
+final class SparkAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
